1322d61e5b360439e8090a8891f6fe8f0938aae2
liliia87 <lilia.krivelyova@gmail.com>
2022-08-28 14:32:03 -0700

    trying to solve the problem with error 415

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -3,8 +3,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CretaceousPark.Models;
-using System;
-using System.Linq;
 
 namespace CretaceousPark.Controllers
 {
@@ -26,6 +24,20 @@ namespace CretaceousPark.Controllers
       return await _db.Animals.ToListAsync();
     }
 
+            // GET: api/Animals/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+        var animal = await _db.Animals.FindAsync(id);
+
+        if (animal == null)
+        {
+            return NotFound();
+        }
+
+        return animal;
+    }
+
     // POST api/animals
     [HttpPost]
     public async Task<ActionResult<Animal>> Post(Animal animal)
@@ -33,7 +45,7 @@ namespace CretaceousPark.Controllers
       _db.Animals.Add(animal);
       await _db.SaveChangesAsync();
 
-      return CreatedAtAction("Post", new { id = animal.AnimalId }, animal);
+      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
     }
 
     // GET: api/Animals/5
@@ -47,42 +59,7 @@ namespace CretaceousPark.Controllers
             return NotFound();
         }
 
-        return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
-    }
-
-    // PUT: api/Animals/5
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Put(int id, Animal animal)
-    {
-      if (id != animal.AnimalId)
-      {
-        return BadRequest();
-      }
-
-      _db.Entry(animal).State = EntityState.Modified;
-
-      try
-      {
-        await _db.SaveChangesAsync();
-      }
-      catch (DbUpdateConcurrencyException)
-      {
-        if (!AnimalExists(id))
-        {
-          return NotFound();
-        }
-        else
-        {
-          throw;
-        }
-      }
-
-      return NoContent();
-    }
-
-    private bool AnimalExists(int id)
-    {
-      return _db.Animals.Any(e => e.AnimalId == id);
+        return animal;
     }
   }
 }
